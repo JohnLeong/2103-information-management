981b6dd52df763bed385eb8e2f7447d8a3266a8c
Teo Li Xien <1902691@sit.singaporetech.edu.sg>
2020-11-04 21:01:25 +0800

    fixed footer cr jialin

--- project/visualisation.php
+++ project/visualisation.php
@@ -44,8 +44,6 @@ if ($connect->connect_error) {
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
         <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-    </head>
-        
         <script type="text/javascript">
         <!-- illustration of bar chart 1 script  -->
                     google.charts.load('current', {
@@ -101,48 +99,52 @@ if ($connect->connect_error) {
         </script>
         <!-- illustration of bar chart 2 script ends  -->  
         
+    </head>
         
-       <!-- Navigation  -->
-       <?php
-       include 'nav.inc.php';
-       ?>
-       <!--Navigation End  -->
 
-       <!-- Banner Section  -->
-       <div class="container-fluid">
-           <div class="row">
-               <div class="jumbotron banner">
-                   <h1 class="text-center banner-text"> DASHBOARD </h1>
-               </div>
-           </div>
-       </div>
-        <!-- Banner Section  -->
-        
-       
-       <body>
-           <div class="container">
-               <div class="row">
-                   <div class ="col-lg-6">
-                       <!-- Illustration of bar chart 1  -->
+    <body>
+        <main>
+            <!-- Navigation  -->
+            <?php
+            include 'nav.inc.php';
+            ?>
+            <!--Navigation End  -->
+
+            <!-- Banner Section  -->
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="jumbotron banner">
+                        <h1 class="text-center banner-text"> DASHBOARD </h1>
+                    </div>
+                </div>
+            </div>
+            <!-- Banner Section  -->
+
+
+
+            <div class="container">
+                <div class="row">
+                    <div class ="col-lg-6">
+                        <!-- Illustration of bar chart 1  -->
                         <div id='colchart_after_1' style='width: 650px; height: 600px; display: inline-block'></div>
-                        
+
                         <!-- table of bar chart 1 Section  -->
                         <div id ="piechart_table">
                             <h4>Average Fee of childcare center parents have to pay based on each type of citizenship (Singaporean, PR, Others)</h4>
                             <table>
-                               <tr>
-                                   <th>types of citizenship</th>
-                                   <th>Average cost</th>
-                               </tr>
+                                <tr>
+                                    <th>types of citizenship</th>
+                                    <th>Average cost</th>
+                                </tr>
 
-                               <?php
-                               require_once('../protected/config.php');
+                                <?php
+                                require_once('../protected/config.php');
                                 $conn = new mysqli(DBHOST, DBUSER, DBPASS, DBNAME);
                                 $sql = "SELECT type_of_citizenship, AVG(fees)
                                 FROM centre_service
                                 WHERE fees IN (SELECT fees FROM centre_service)
                                 GROUP BY type_of_citizenship
-                                ORDER BY AVG(fees) ASC;"; 
+                                ORDER BY AVG(fees) ASC;";
 
                                 $fire = mysqli_query($conn, $sql);
 
@@ -150,61 +152,66 @@ if ($connect->connect_error) {
                                     $errorMsg = "Connection failed: " . $conn->connect_error;
                                     $success = false;
                                 }
-                               while ($result = mysqli_fetch_assoc($fire)){
-                                echo "<tr><td>".$result["type_of_citizenship"]."</td><td>$".$result["AVG(fees)"]."</td></tr>";
-
+                                while ($result = mysqli_fetch_assoc($fire)) {
+                                    echo "<tr><td>" . $result["type_of_citizenship"] . "</td><td>$" . $result["AVG(fees)"] . "</td></tr>";
                                 }
                                 echo "</table>";
-                              ?>
+                                ?>
                             </table>
                             <!-- table of bar chart 1 Section ends -->
                         </div> 
-                        
-           
-                   </div>
-                   
-                   <div class ="col-lg-6">
-                       <!-- Illustration of of bar chart 2 Section  -->
-                       <div id='colchart_after' style='width: 650px; height: 600px; display: inline-block'></div>
-                       
-                            <!-- table of bar chart 2 Section  -->
-                            <div id ="piechart_table">
-                                <h4>Name of childcare center that does not offer food for the child according to the type of service (full day services, half day services etc)</h4>
-                                <table id="example" class="display">
-                                   <tr>
-                                       <th>Name of Centers </th>
-                                       <th>Type of Services</th>
-                                   </tr>
 
-                                   <?php
-                                   require_once('../protected/config.php');
-                                    $conn = new mysqli(DBHOST, DBUSER, DBPASS, DBNAME);
-                                    $sql = "SELECT DISTINCT centre.centre_name AS name, type_of_service
+
+                    </div>
+
+                    <div class ="col-lg-6">
+                        <!-- Illustration of of bar chart 2 Section  -->
+                        <div id='colchart_after' style='width: 650px; height: 600px; display: inline-block'></div>
+
+                        <!-- table of bar chart 2 Section  -->
+                        <div id ="piechart_table">
+                            <h4>Name of childcare center that does not offer food for the child according to the type of service (full day services, half day services etc)</h4>
+                            <table id="example" class="display">
+                                <tr>
+                                    <th>Name of Centers </th>
+                                    <th>Type of Services</th>
+                                </tr>
+
+                                <?php
+                                require_once('../protected/config.php');
+                                $conn = new mysqli(DBHOST, DBUSER, DBPASS, DBNAME);
+                                $sql = "SELECT DISTINCT centre.centre_name AS name, type_of_service
                                             FROM centre
                                             INNER JOIN centre_service ON centre.centre_code=centre_service.centre_code
                                             WHERE centre.food_offered = 'na'
-                                            ORDER BY type_of_service;"; 
+                                            ORDER BY type_of_service;";
 
-                                    $fire = mysqli_query($conn, $sql);
+                                $fire = mysqli_query($conn, $sql);
 
-                                    if ($conn->connect_error) {
-                                        $errorMsg = "Connection failed: " . $conn->connect_error;
-                                        $success = false;
-                                    }
-                                   while ($result = mysqli_fetch_assoc($fire)){
-                                       
-                                        echo "<tr><td>".$result["name"]."</td><td>".$result["type_of_service"]."</td></tr>";
+                                if ($conn->connect_error) {
+                                    $errorMsg = "Connection failed: " . $conn->connect_error;
+                                    $success = false;
+                                }
+                                while ($result = mysqli_fetch_assoc($fire)) {
 
-                                    }
-                                    echo "</table>";
-                                  ?>
-                                </table>        
-                                <!-- table of bar chart 2 Section ends -->
-                            </div>  
-                   </div>
-               </div>  
-           </div>       
-       </body>
+                                    echo "<tr><td>" . $result["name"] . "</td><td>" . $result["type_of_service"] . "</td></tr>";
+                                }
+                                echo "</table>";
+                                ?>
+                            </table>        
+                            <!-- table of bar chart 2 Section ends -->
+                        </div>  
+                    </div>
+                </div>  
+            </div>     
+
+            <!--Footer-->
+<?php
+include 'footer.inc.php';
+?>
+            <!--Footer End-->
+        </main>
+    </body>
 </html>
 
 
